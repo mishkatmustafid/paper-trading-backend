e8d9aace7d902c80811b01a2ed82ef622227aea0
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-06-28 11:03:35 +0600

    api/v1 init

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,9 +4,13 @@ Router modules import
 
 from fastapi import APIRouter
 
-from app.api.v1 import home
+from app.api.v1 import (
+    home,
+    user,
+)
 
 api_router = APIRouter()
 
 # Root router
 api_router.include_router(home.router, prefix="/home", tags=["general"])
+api_router.include_router(user.router, prefix="/users", tags=["users"])
