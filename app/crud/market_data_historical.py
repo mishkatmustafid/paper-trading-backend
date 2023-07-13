1d2a650b4ae5f241f3f6a711cedaa1df48b8720d
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-07-13 18:26:42 +0600

    MarketData crud updated

--- app/crud/market_data_historical.py
+++ app/crud/market_data_historical.py
@@ -5,6 +5,8 @@ MarketDataHistorical CRUD operations
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+from app.models import MarketDataHistorical, asset_type
+
 
 class CRUDMarketDataHistorical(BaseModel):
     """
@@ -12,8 +14,16 @@ class CRUDMarketDataHistorical(BaseModel):
     """
 
     @staticmethod
-    def get_price_by_symbol(db: Session, symbol: str):
-        return {"symbol": symbol, "price": 234.56}
+    def get_price_by_symbol(db: Session, symbol: str, asset_type: asset_type):
+        details = MarketDataHistorical.get_market_data_historical_by_symbol(
+            db, symbol, asset_type
+        )
+        open = details["open"]
+        high = details["high"]
+        low = details["low"]
+        close = details["close"]
+        price = (open + high + low + close) / 4.0
+        return price
 
 
 market_data_historical = CRUDMarketDataHistorical()
