5411f569dc9a28edf4e843ee2e904738d9c63e0d
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-07-27 09:22:27 +0600

    market_data_historical model updated

--- app/models/market_data_historical.py
+++ app/models/market_data_historical.py
@@ -1,15 +1,13 @@
 """
 Market Data Historical models module
 """
-import enum
 import uuid
 
-from sqlalchemy import Column, Date, Enum, Float, Identity, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Float, Identity, Integer
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, relationship
 
 from app.models import Base
-from app.models.asset_type import AssetType
 
 
 class MarketDataHistorical(Base):
@@ -36,15 +34,18 @@ class MarketDataHistorical(Base):
         nullable=False,
         index=True,
     )
-    date = Column(Date, nullable=False)
-    name: str = Column(String(50), nullable=False)
-    asset_type: enum = Column(Enum(AssetType), nullable=False)
-    symbol: str = Column(String(50), nullable=False, index=True)
+    asset_id: str = Column(
+        UUID(as_uuid=True), ForeignKey("assets.asset_id"), nullable=False
+    )
+    datetime: datetime = Column(DateTime, nullable=False)
     open: float = Column(Float, nullable=False)
     high: float = Column(Float, nullable=False)
     low: float = Column(Float, nullable=False)
     close: float = Column(Float, nullable=False)
     volume: int = Column(Integer, nullable=False)
+
+    # Relationships
+    asset = relationship("Assets", back_populates="market_data_historical")
 
     @classmethod
     def get_market_data_historical_by_market_data_historical_id(
@@ -61,33 +62,13 @@ class MarketDataHistorical(Base):
         )
 
     @classmethod
-    def get_market_data_historical_by_symbol(
-        cls, db: Session, symbol: str, asset_type: AssetType
-    ):
+    def get_by_asset_id(cls, db: Session, asset_id: str):
         """
-        Gets all historical market data from database based on given symbol and asset type.
+        Gets historical data based on given asset id
         """
-
         return (
             db.query(MarketDataHistorical)
             .where(MarketDataHistorical.deleted_at == None)
-            .filter(MarketDataHistorical.symbol == symbol)
-            .filter(MarketDataHistorical.asset_type == asset_type)
-            .first()
-        )
-
-    @classmethod
-    def get_market_data_historical_by_name(
-        cls, db: Session, name: str, asset_type: AssetType
-    ):
-        """
-        Gets all historical market data from database based on given name and asset type.
-        """
-
-        return (
-            db.query(MarketDataHistorical)
-            .where(MarketDataHistorical.deleted_at == None)
-            .filter(MarketDataHistorical.name == name)
-            .filter(MarketDataHistorical.asset_type == asset_type)
-            .first()
+            .filter(MarketDataHistorical.asset_id == asset_id)
+            .all
         )
