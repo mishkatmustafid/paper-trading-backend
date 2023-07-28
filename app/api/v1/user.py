0e281945d9ae6ef15decaea2e684e913a13675dc
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-07-28 17:11:48 +0600

    user Endpoints updated

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -21,11 +21,14 @@ from app.schemas.user import (
     GetUserResponse,
     SignInUser,
     SignInUserResponse,
+    UpdateUser,
+    UpdateUserResponse,
 )
 from app.utils import handle_error
 from app.utils.exceptions import (
     CustomAuthError,
     CustomTokenError,
+    DatabaseOperationError,
     InvalidUUIDError,
     SQLAlchemyObjectNotFoundError,
 )
@@ -186,6 +189,94 @@ async def get_user_details(
         # ---------------------------------------------------
         raise CustomAuthError(
             "You are not authorized to view this user details.", status_code=401
+        )
+
+    except Exception as err:
+        logger.error(err)
+        return handle_error.send_error(err, response)
+
+
+@router.put(
+    "/{user_id}/updates",
+    dependencies=[Depends(JWTBearer())],
+    response_model=UpdateUserResponse,
+    response_model_exclude_unset=True,
+)
+async def update_user_details(
+    user_id: str,
+    payload: UpdateUser,
+    request: Request,
+    response: Response,
+    db: Session = Depends(db_connection),
+) -> Any:
+    """
+    Updates a user details
+    """
+
+    try:
+        if not is_valid_uuid(user_id):
+            raise InvalidUUIDError(
+                "Make sure to provide user uuid in a valid UUID format.",
+                status_code=400,
+            )
+
+        user_details = verify_gty(db, request, User, user_id)
+
+        if user_details["status"]:
+            update_details = jsonable_encoder(crud.user.update(db, user_id, payload))
+            update_details = General.exclude_metadata(update_details)
+
+            if update_details:
+                return {
+                    "status": True,
+                    "message": "Successfully updated the user!",
+                    "details": update_details,
+                }
+            # fixme: check what are possible errors and pass the error message accordingly.
+            raise DatabaseOperationError("Database operation error")
+
+        raise CustomAuthError(
+            "You are unauthorized to update this user details.",
+            status_code=401,
+        )
+
+    except Exception as err:
+        logger.error(err)
+        return handle_error.send_error(err, response)
+
+
+@router.delete(
+    "/{user_id}",
+    dependencies=[Depends(JWTBearer())],
+    response_model=UpdateUserResponse,
+    response_model_exclude_unset=True,
+)
+async def delete_a_user(
+    user_id: str,
+    request: Request,
+    response: Response,
+    db: Session = Depends(db_connection),
+) -> Any:
+    """
+    Deletes a user.
+    """
+
+    try:
+        if not is_valid_uuid(user_id):
+            raise InvalidUUIDError(
+                "Make sure to provide user uuid in a valid UUID format.",
+                status_code=400,
+            )
+
+        user_details = verify_gty(db, request, User, user_id)
+
+        if user_details["status"]:
+            user_details = crud.user.delete(db, user_id)
+            if user_details:
+                return {"status": True, "message": "Successfully deleted the user!"}
+
+        raise CustomAuthError(
+            "You are unauthorized to delete this user details.", status_code=401
         )
 
     except Exception as err:
