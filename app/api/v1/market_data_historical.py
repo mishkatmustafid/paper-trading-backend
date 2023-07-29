997c88d2ca7f47477b0b2b0e3d9690c1a08e6a65
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-07-29 20:58:56 +0600

    market_data_historical endpoint

--- app/api/v1/market_data_historical.py
+++ app/api/v1/market_data_historical.py
@@ -4,120 +4,82 @@ contains handlers related to fetch historical data of
 symbols
 """
 
+from typing import Any
+
 from fastapi import APIRouter, Depends, Response
+from fastapi.encoders import jsonable_encoder
 from loguru import logger
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.auth.bearer import JWTBearer
 from app.db.session import db_connection
-from app.schemas import GetPriceResponse
+from app.schemas import (
+    CreateMarketDataHistorical,
+    CreateMarketDataHistoricalResponse,
+    GetAllDataResponse,
+)
+from app.utils.general import General
 from app.utils.handle_error import handle_error
 
 router = APIRouter()
 
 
-@router.get(
+@router.post(
     "",
     dependencies=[Depends(JWTBearer())],
-    response_model=GetPriceResponse,
+    response_model=CreateMarketDataHistoricalResponse,
     response_model_exclude_unset=True,
 )
-async def get_price(
-    symbol: str,
+async def create_data(
+    payload: CreateMarketDataHistorical,
     response: Response,
     db: Session = Depends(db_connection),
-):
+) -> Any:
     """
-    Get price of a symbol
+    Create new historical market data
     """
     try:
-        if details := crud.market_data.get_price_by_symbol(db, symbol):
-            return {
-                "status": True,
-                "message": "Successfully got the price!",
-                "details": details,
-            }
+        market_data_historical = General.exclude_metadata(
+            jsonable_encoder(crud.market_data_historical.create(db, payload))
+        )
         return {
-            "status": False,
-            "message": "No price found for the given symbol!",
-            "details": {},
+            "status": True,
+            "message": "Successfully created marketdata historical",
+            "details": market_data_historical,
         }
+
     except Exception as err:
         logger.error(err)
         return handle_error.send_error(err, response)
 
 
-# @router.get(
-#     "/{symbol}/details",
-#     dependencies=[Depends(JWTBearer())],
-#     response_model=GetDetailsResponse,
-#     response_model_exclude_unset=True,
-# )
-# def get_details(
-#     symbol: str,
-#     response: Response,
-#     db: Session = Depends(db_connection),
-# ) -> Any:
-#     """
-#     Get details of a symbol
-#     """
-#     try:
-
-#           details = crud.market_data.get_details_by_symbol(db, symbol)
-
-#           return {
-#               "status": True,
-#               "message": "Successfully got the details!",
-#               "details": {
-#                   "symbol": symbol,
-#                   "open": details["open"],
-#                   "high": details["high"],
-#                   "low": details["low"],
-#                   "volume": details["volume"],
-#                   "52_week_high": details["52_week_high"],
-#                   "52_week_low": details["52_week_low"],
-#                   "market_cap": details["market_cap"],
-#               },
-#           }
-#     except Exception as err:
-#         logger.error(err)
-#         return handle_error.send_error(err, response)
-
-
-# @router.get(
-#     "/klines",
-#     dependencies=[Depends(JWTBearer())],
-#     response_model=GetKlinesResponse,
-#     response_model_exclude_unset=True,
-# )
-# def get_details(
-#     symbol: str,
-#     interval: str,
-#     start_time: int,
-#     end_time: int,
-#     limit: int,
-#     response: Response,
-#     db: Session = Depends(db_connection),
-# ) -> Any:
-#     """
-#     Get klines of a symbol
-#     """
-#     try:
-#         klines = crud.market_data.get_klines(
-#             db, symbol, interval, start_time, end_time, limit
-#         )
-
-#         return {
-#             "status": True,
-#             "message": "Successfully got the klines!",
-#             "details": {
-#                 "symbol": symbol,
-#                 "interval": interval,
-#                 "limit": limit,
-#                 "klines": klines,
-#             },
-#         }
-#     except Exception as err:
-#         logger.error(err)
-#         return handle_error.send_error(err, response)
+@router.get(
+    "/{asset_id}",
+    dependencies=[Depends(JWTBearer())],
+    response_model=GetAllDataResponse,
+    response_model_exclude_unset=True,
+)
+async def get_info(
+    asset_id: str,
+    response: Response,
+    db: Session = Depends(db_connection),
+) -> Any:
+    """
+    Get data of an asset
+    """
+    try:
+        if details := crud.market_data_historical.get_by_asset_id(db, asset_id):
+            return {
+                "status": True,
+                "message": "Successfully got the asset data!",
+                "details": details,
+            }
+        return {
+            "status": False,
+            "message": "No data found for the given asset!",
+            "details": {},
+        }
+    except Exception as err:
+        logger.error(err)
+        return handle_error.send_error(err, response)
