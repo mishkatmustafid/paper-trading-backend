e761f3d2a3154a70f582216f16345ce867e0bc3f
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-07-29 19:47:27 +0600

    market_data_historical CRUD - updated

--- app/crud/market_data_historical.py
+++ app/crud/market_data_historical.py
@@ -2,10 +2,15 @@
 MarketDataHistorical CRUD operations
 """
 
+from datetime import datetime
+from typing import Any, Dict, Union
+
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
-from app.models import MarketDataHistorical, asset_type
+from app.models import MarketDataHistorical
+from app.schemas.market_data_historical import CreateMarketDataHistorical
 
 
 class CRUDMarketDataHistorical(BaseModel):
@@ -14,16 +19,66 @@ class CRUDMarketDataHistorical(BaseModel):
     """
 
     @staticmethod
-    def get_price_by_symbol(db: Session, symbol: str, asset_type: asset_type):
-        details = MarketDataHistorical.get_market_data_historical_by_symbol(
-            db, symbol, asset_type
+    def create(db: Session, payload_in: CreateMarketDataHistorical):
+        serialized_data = jsonable_encoder(payload_in)
+        db_obj = MarketDataHistorical(**serialized_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+
+        return db_obj
+
+    @staticmethod
+    def get_by_market_data_historical_id(db: Session, market_data_historical_id: str):
+        return MarketDataHistorical.get_market_data_historical_by_market_data_historical_id(
+            db, market_data_historical_id
         )
-        open = details["open"]
-        high = details["high"]
-        low = details["low"]
-        close = details["close"]
-        price = (open + high + low + close) / 4.0
-        return price
+
+    @staticmethod
+    def get_by_asset_id(db: Session, asset_id: str):
+        return MarketDataHistorical.get_by_asset_id(db, asset_id)
+
+    @staticmethod
+    def update(
+        db: Session,
+        market_data_historical_id: str,
+        obj_in: Union[MarketDataHistorical, Dict[str, Any]],
+    ):
+        """
+        Updates attibutes of the given marketdata historical
+        """
+
+        # retrieve details of the given portfolio
+        if market_data_historical_details := MarketDataHistorical.get_by_market_data_historical_id(
+            db, market_data_historical_id
+        ):
+            obj_data = jsonable_encoder(market_data_historical_details)
+
+            # check if the given payload (i.e. obj_in) is in a
+            # dictionary instance
+            if isinstance(obj_in, dict):
+                update_data = obj_in
+            else:
+                # only payload is set to be updated
+                update_data = obj_in.dict(exclude_unset=True)
+
+            # update only those attributes sent via payload
+            for field in update_data:
+                if field in obj_data:
+                    setattr(market_data_historical_details, field, update_data[field])
+
+            db.add(market_data_historical_details)
+            db.commit()
+            db.refresh(market_data_historical_details)
+
+    def delete(self, db: Session, market_data_historical_id: str):
+        # Delete performs a soft delete i.e. deleted_at attribute
+        # is updated with a timestamp
+        # Data would still be in the database but won't show up in
+        # query result
+        payload = {"deleted_at": datetime.utcnow()}
+
+        return self.update(db, market_data_historical_id, payload)
 
 
 market_data_historical = CRUDMarketDataHistorical()
